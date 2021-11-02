e594b8fea1aab1d47028f30b272100eed32dabe5
Davidmarmit <davidmartinezmit@gmail.com>
2021-11-02 10:47:20 +0000

    Llibreries

---
+++ Practica/fase1.c
@@ -0,0 +1,9 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/gpio.h>
+#include <linux/interrupt.h>
+#include <linux/kobject.h>
+#include <linux/time.h>
+
+
