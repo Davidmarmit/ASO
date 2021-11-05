aeca2019ac8f52017a7510e5eae5db38cc1c385b
Davidmarmit <davidmartinezmit@gmail.com>
2021-11-05 11:40:34 +0000

    Solucionat problema gpioButton

--- Practica/fase1.c
+++ Practica/fase1.c
@@ -187,7 +187,7 @@ static void __exit ebbgpio_exit(void){
 static irq_handler_t ebbgpio_irq1_handler(unsigned int irq, void *dev_id, struct pt_regs *regs){
    ledOn1 = 1;                          // Invert the LED state on each button press
    gpio_set_value(gpioLED1, ledOn1);          // Set the physical LED accordingly
-   printk(KERN_INFO "GPIO_TEST: Interrupt! (button state is %d)\n", gpio_get_value(gpioButton));
+   printk(KERN_INFO "GPIO_TEST: Interrupt! (button state is %d)\n", gpio_get_value(gpioButtonA1));
    numberPressesA1++;                         // Global counter, will be outputted when the module is unloaded
    return (irq_handler_t) IRQ_HANDLED;      // Announce that the IRQ has been handled correctly
 }
